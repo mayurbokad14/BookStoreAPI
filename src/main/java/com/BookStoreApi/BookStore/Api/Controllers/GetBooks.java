59e3e1748720506efcc06f0fb7c5fc051a1b693f
Mayur Bokad <mayurbokad8@gmail.com>
2023-07-03 18:58:29 +0530

    redirecting and limiting the page

--- src/main/java/com/BookStoreApi/BookStore/Api/Controllers/GetBooks.java
+++ src/main/java/com/BookStoreApi/BookStore/Api/Controllers/GetBooks.java
@@ -1,12 +1,13 @@
 package com.BookStoreApi.BookStore.Api.Controllers;
 
-import org.springframework.http.HttpHeaders;
 import java.util.ArrayList;
-
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.BookStoreApi.BookStore.Api.Models.Book;
 import com.BookStoreApi.BookStore.Api.Models.BooksRepository;
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 @CrossOrigin
 @RestController
@@ -27,38 +29,35 @@ public class GetBooks {
     private BooksRepository booksRepository;
 
     @GetMapping("/v1/api/book")
-    public ResponseEntity<String> getBooks(@RequestParam(required = false) String name, @RequestParam(required = false) Integer maxItems ){
+    public ResponseEntity<String> getBooks(
+        @RequestParam(required = false) String name,
+        @RequestParam(required = false) Integer maxItems,
+        @RequestParam(required = false) Integer pageNum ){
 
-        List<Book> books=new ArrayList<Book>();
+        int maxItemsPerPage = maxItems == null ? 10 : maxItems;
+        int pageNumber = pageNum == null ? 0 : (pageNum > 0 ? pageNum -1 : 0);
+        Pageable limit = PageRequest.of(pageNumber, maxItemsPerPage);
 
-        
-        if(name == null && maxItems == null){
-            //name and maxItems parameter is missing
-            booksRepository.findAll().forEach(books::add);
-        }
-        else if(name != null && maxItems == null){
-            //name is present, but maxItems parameter is missing
-            booksRepository.findByNameContainingIgnoreCase(name).forEach(books::add);
-        }
-        else if( name == null && maxItems !=null){
-            //name is missing but maxItems parameter is present
-            //select * from books limit `maxItems`
-            Pageable limit = PageRequest.of(0,maxItems);
-            booksRepository.findAll(limit).forEach(books::add);
+        Page<Book> page;
+        Gson gson = new Gson();
+
+        if(name == null){
+            page = booksRepository.findAll(limit);
         }
         else{
-            // both name and maxItems paramter is present
-            //select * from books where name like '%name%' limit `maxItems`
-            booksRepository.findByNameContainingIgnoreCase(name).forEach(books::add);
+            page = booksRepository.findByNameContainingIgnoreCase(name, limit);
         }
-        
 
-        Gson gson = new Gson();
+        //int totalPages = page.getTotalPages();
+        //JsonObject responseData = new JsonObject();
+
+        //responseData.addProperty("totalPages", totalPages);
+        //responseData.addProperty("content", gson.toJson(page));
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
-        return new ResponseEntity<String>(gson.toJson(books), headers, HttpStatus.OK);
+        return new ResponseEntity<String>(gson.toJson(page), headers, HttpStatus.OK);
     }
     
 }
--- src/main/java/com/BookStoreApi/BookStore/Api/Models/BooksRepository.java
+++ src/main/java/com/BookStoreApi/BookStore/Api/Models/BooksRepository.java
@@ -2,10 +2,14 @@ package com.BookStoreApi.BookStore.Api.Models;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BooksRepository extends JpaRepository <Book, Long>{
 
     List<Book> findByNameContainingIgnoreCase(String name);
+
+    Page<Book> findByNameContainingIgnoreCase(String name, Pageable limit);
 
 }
