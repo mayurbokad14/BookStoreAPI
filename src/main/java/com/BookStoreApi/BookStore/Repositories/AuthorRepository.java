3669f2adc83795c532c7e47896b1fd205c1866ef
Mayur Bokad <mayurbokad8@gmail.com>
2023-07-20 21:27:16 +0530

    add authorrepository in author controller

--- src/main/java/com/BookStoreApi/BookStore/Repositories/AuthorRepository.java
+++ src/main/java/com/BookStoreApi/BookStore/Repositories/AuthorRepository.java
@@ -3,5 +3,8 @@ package com.BookStoreApi.BookStore.Repositories;
 import com.BookStoreApi.BookStore.Models.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+    List<Author> findByNameContainingIgnoreCase(String name);
 }
