7d3f4fb286cde595fc79f188f330f7ec199a63d3
Mayur Bokad <mayurbokad8@gmail.com>
2023-07-20 21:28:51 +0530

    add customer repository in customer controller

--- src/main/java/com/BookStoreApi/BookStore/Repositories/CustomerRepository.java
+++ src/main/java/com/BookStoreApi/BookStore/Repositories/CustomerRepository.java
@@ -3,5 +3,9 @@ package com.BookStoreApi.BookStore.Repositories;
 import com.BookStoreApi.BookStore.Models.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    List<Customer> findByNameContainingIgnoreCase(String name);
 }
